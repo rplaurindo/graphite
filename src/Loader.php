2a4477f0da631ecb133e5c99002c8c53e3e97661
Rafael Pereira Laurindo <rplaurindo>
2018-06-30 20:40:42 -0300

    refactoring

--- src/Loader.php
+++ src/Loader.php
@@ -7,23 +7,21 @@ use GraphIte\Directory;
 class Loader {
 
     private $className;
-    private $namespacedPath;
-    private $paths;
-    private $directoryIterator;
 
     function __construct($className) {
         $this->className = $className;
-        $this->paths = explode(PATH_SEPARATOR, get_include_path());
-        $this->namespacedPath =  str_replace('\\', DIRECTORY_SEPARATOR, $className)  . '.php';
-        $this->directoryIterator = new Directory($this->paths);
     }
 
     function load() {
-        foreach ($this->directoryIterator as $path) {
+        $paths = explode(PATH_SEPARATOR, get_include_path());
+        $directoryIterator = new Directory($paths);
 
-            $absolutePath = $path . DIRECTORY_SEPARATOR . $this->namespacedPath;
+        foreach ($directoryIterator as $path) {
+
+            $namespacedPath =  str_replace('\\', DIRECTORY_SEPARATOR, $this->className)  . '.php';
+            $absolutePath = $path . DIRECTORY_SEPARATOR . $namespacedPath;
             if (file_exists($absolutePath)) {
-                include $this->namespacedPath;
+                include $namespacedPath;
                 break;
             } else {
                 $fileName = basename($this->className)  . '.php';

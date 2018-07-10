782603dc0f6995ed1d4a0e45b3ee6dbda8d35034
Rafael <rafaelplaurindo@gmail.com>
2018-07-10 08:14:15 -0300

    Update test-directories.php

--- test-directories.php
+++ test-directories.php
@@ -1,14 +1,9 @@
 <?php
 
-//require_once('./load-paths.php');
-//require_once('./autoloader.php');
-
-use GraphIte\Directory;
-
 $paths = explode(PATH_SEPARATOR, get_include_path());
 $paths = ['C:'];
 
-$directoryIterator = new Directory($paths);
+$directoryIterator = new GraphIte\Directory($paths);
 
 foreach ($directoryIterator as $path) {
     echo $path . "\n";
